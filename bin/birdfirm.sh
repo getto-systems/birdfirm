c6f4b9136adb9383ceeace154da506ac3f98730d
shun <shun.fix9@gmail.com>
2016-09-12 17:45:29 +0900

    セッションが存在する場合マークを表示するように修正

--- bin/birdfirm.sh
+++ bin/birdfirm.sh
@@ -3,17 +3,27 @@
 birdfirm_main(){
   local root
   local hostfile
+  local session
   local path
   root=$HOME/.birdfirm/hosts
 
   if [ -d "$root" ]; then
-    hostfile=$(ls -1 $root | peco)
-    if [ "$hostfile" ]; then
-      path=$root/$hostfile
-      if [ -f "$path" ]; then
-        $path
+    while [ true ]; do
+      ls -1 $root | awk '{print "  " $1}' > $root.txt
+      for session in $(tmux -S $HOME/.tmux.wrapper/tmux.sock ls -F '#S' 2> /dev/null); do
+        sed "s/^  $session$/* $session/" -i $root.txt
+      done
+      hostfile=$(cat $root.txt | peco)
+      hostfile=${hostfile##* }
+      if [ "$hostfile" ]; then
+        path=$root/$hostfile
+        if [ -f "$path" ]; then
+          $path
+        fi
+      else
+        return
       fi
-    fi
+    done
   fi
 }
 
