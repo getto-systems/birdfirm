8779a566c3e9f88089be52757ba325ec466c831c
shun <shun.fix9@gmail.com>
2016-09-30 12:54:20 +0900

    fix for docker session

--- bin/birdfirm.sh
+++ bin/birdfirm.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 birdfirm_main(){
+  local birdfirm_root
   local root
   local hosts
   local hostfile
-  local socket
-  local session
   local path
-  root=$HOME/.birdfirm/hosts
+  birdfirm_root=$HOME/.birdfirm
+  root=$birdfirm_root/hosts
   hosts=$root.txt
 
   trap birdfirm_hungup 1
@@ -19,12 +19,7 @@ birdfirm_main(){
 
   if [ -d "$root" ]; then
     while [ true ]; do
-      ls -1 $root | awk '{print "  " $1}' > $hosts
-      for socket in $HOME/.tmux.wrapper/tmux-*.sock; do
-        for session in $(tmux -S $socket ls -F '#S' 2> /dev/null); do
-          sed "s/^  $session$/* $session/" -i $hosts
-        done
-      done
+      birdfirm_build_hosts
       hostfile=$(cat $hosts | peco)
       hostfile=${hostfile##* }
       if [ "$hostfile" ]; then
@@ -38,6 +33,30 @@ birdfirm_main(){
       fi
     done
   fi
+}
+birdfirm_build_hosts(){
+  ls -1 $root | awk '{print "  " $1}' > $hosts
+  if [ "$(which tmux)" ]; then
+    birdfirm_build_hosts_tmux
+  fi
+  if [ "$(which docker)" ]; then
+    birdfirm_build_hosts_docker
+  fi
+}
+birdfirm_build_hosts_tmux(){
+  local socket
+  local session
+  for socket in $HOME/.tmux.wrapper/tmux-*.sock; do
+    for session in $(tmux -S $socket ls -F '#S' 2> /dev/null); do
+      sed "s/^  $session$/* $session/" -i $hosts
+    done
+  done
+}
+birdfirm_build_hosts_docker(){
+  local session
+  for session in $(docker ps --format "{{.Names}}"); do
+    sed "s/^  $session$/* $session/" -i $hosts
+  done
 }
 birdfirm_hungup(){
   birdfirm_cleanup
